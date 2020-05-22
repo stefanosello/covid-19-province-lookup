db99df1077032ea7430a29f2450a1dc27086b0b4
Stefano Sello <sellostefano@gmail.com>
2020-05-22 17:06:12 +0200

    Geo data api improvements + added lodash

--- app/controllers/api/v1/geo_data_controller.rb
+++ app/controllers/api/v1/geo_data_controller.rb
@@ -1,26 +1,28 @@
 class Api::V1::GeoDataController < ApplicationController
+
   def get
-    response = {}
-    if params['region'].present? 
-      data = Region.includes(:provinces).find(params['region'])
-      response = data.as_json
-      response['provinces'] = data.provinces.as_json
-    else
-      data = Nation.includes(regions: :provinces)
-      if params['nation'].present?
-        data = [data.find(params['nation'])]
-      else
-        data = data.all
-      end
-      data.each do |nation|
-        response[] << nation.as_json
-        response["regions"] = data.regions.map do |region|
-          region_data = region.as_json
-          region_data["provinces"] = region.provinces.as_json
-          region_data
-        end
-      end
+    response = []
+    nation_code = params["nation"].upcase rescue nil
+    region_label = params["region"].capitalize rescue nil
+    
+    nations = Nation.includes(regions: :provinces)
+    if nation_code.present?
+      nations = nations.where(code: nation_code)
     end
+    if region_label.present?
+      nations = nations.where(regions: {label: region_label})
+    end
+    
+    nations.each do |nation|
+      nation_converted = nation.as_json
+      nation_converted["regions"] = nation.regions.map do |region|
+        region_converted = region.as_json
+        region_converted["provinces"] = region.provinces.map {|province| province.as_json}
+        region_converted
+      end
+      response << nation_converted
+    end
+
 
     respond_to do |format|
       format.json { render :json => response }
