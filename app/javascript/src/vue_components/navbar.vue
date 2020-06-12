1f4b346d4763a90562df694af0b6153977f01987
Stefano Sello <sellostefano@gmail.com>
2020-06-12 08:52:49 +0200

    Added icon in navbar

--- app/javascript/src/vue_components/navbar.vue
+++ app/javascript/src/vue_components/navbar.vue
@@ -12,7 +12,10 @@ export default {
 
 <template>
   <div class="w-100 px-3 d-flex justify-content-between align-items-center sticky-top" navbar-component>
-    <h1><div class="font-weight-bold">Covid-19</div> <div class="small">Province stats</div></h1>
+    <div class="d-flex align-items-center">
+      <img src="/favicon.png" class="icon"/>
+      <h1 class="ml-2"><div class="font-weight-bold">Covid-19</div> <div class="small">Province stats</div></h1>
+    </div>
     <button class="btn btn-dark btn-sm font-weight" @click="$emit('open-sidebar')">IMPOSTAZIONI</button>
   </div>
 </template>
@@ -25,6 +28,11 @@ export default {
 
   h1 {
     font-size: 1rem;
+  }
+
+  .icon {
+    max-height: calc(#{$header-height} - 2rem);
+    width: auto;
   }
 }
 </style>
